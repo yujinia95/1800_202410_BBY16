ebd18c025fb2e72cd9b2e512cdedf069375bd1ed
farzdaffarzin <ffarzin3@my.bcit.ca>
2024-03-23 12:09:28 -0700

    profile page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,6 +1,12 @@
-function personalFunction(){
+function profileFunction(){
+    window.location.href = 'profile.html'
+}
+
+function homeFunction(){
+    window.location.href = 'main.html'
+}
+
+function diaryFunction(){
+    window.location.href = 'diary.html'
 
 }
-function diaryFunction(){
-}
-  
